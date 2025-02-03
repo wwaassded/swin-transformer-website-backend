ebbd1b4da2d03de05591f8116b03b7ebea894cf8
wwaassded <wwssd507@gmail.com>
2025-02-03 21:17:07 +0800

    swin-celery

--- swinTransformer/views/celery_view.py
+++ swinTransformer/views/celery_view.py
@@ -1,3 +1,4 @@
+import json
 from celery.result import AsyncResult
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -6,5 +7,22 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(['GET'])
 @csrf_exempt
-def get_celery_task_status_and_result(request, task_id: int):
-    pass
+def get_celery_task_status_and_result(request, task_id: str):
+    result = AsyncResult(task_id)
+    if result.failed():
+        return JsonResponse({
+            'isDone': True,
+            'isSuccess': False,
+            'traceBack': result.traceback
+        })
+    if result.successful():
+        return JsonResponse({
+            'isDone': True,
+            'isSuccess': True,
+            'result': json.loads(result.get())
+        })
+    else:
+        return JsonResponse({
+            'isDone': False,
+            'isSuccess': False,
+        })
--- swinTransformer/views/image_view.py
+++ swinTransformer/views/image_view.py
@@ -41,7 +41,7 @@ def upload_file(request):
         return JsonResponse({'isSuccessful': False, 'message': 'io error on server'})
     source_image_url = nginx_image_url_root + file.name  # 用户上传照片的url地址
     result = swin_transformer_process_image.delay(user_id, source_image_url, file.name)
-    task_id = result.id
+    task_id: str = result.id
     return JsonResponse({
         'isSuccessful': True,
         'task_id': task_id,
