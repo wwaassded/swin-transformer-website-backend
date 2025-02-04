99067b2ccf0166b5d9793e5b03aef87eefab2461
wwaassded <wwssd507@gmail.com>
2025-02-04 11:19:04 +0800

    swin-celery ready TODO:consumer

--- swinTransformer/routing.py
+++ swinTransformer/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from swinTransformer import consumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/some_path/$', consumer.TaskConsumer.as_asgi(), name='get_task_status_and_result')
+    re_path(r'ws/get_task_satsus/$', consumer.TaskConsumer.as_asgi(), name='get_task_status_and_result')
 ]
