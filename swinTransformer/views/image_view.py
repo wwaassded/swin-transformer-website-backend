67c809f8fa79e27d4dcdf0006d55bfeea343171c
wwaassded <wwssd507@gmail.com>
2025-02-04 13:17:06 +0800

    swin-celery done without any test🤞

--- swinTransformer/views/image_view.py
+++ swinTransformer/views/image_view.py
@@ -41,8 +41,8 @@ def upload_file(request):
         print(f'Error:{e}')
         return JsonResponse({'isSuccessful': False, 'message': 'io error on server'})
     source_image_url = nginx_image_url_root + file.name  # 用户上传照片的url地址
-    result = swin_transformer_process_image.delay(uuid.uuid4(), user_id, source_image_url, file.name)
-    task_id: str = result.id
+    task_id: str = str(uuid.uuid4())
+    swin_transformer_process_image.delay(task_id, user_id, source_image_url, file.name)
     return JsonResponse({
         'isSuccessful': True,
         'task_id': task_id,
